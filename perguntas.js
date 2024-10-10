6411d67f729f465b7e26b2631cafbb003e8a448c
kkjcordeiro <162117954+kkjcordeiro@users.noreply.github.com>
2024-10-10 07:51:26 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano foi declarada a independencia?',
+    '1822',
+)
 
+criaCartao(
+    'Matemática',
+    'Quanto é 10x10?',
+    '100',
+)
+
+criaCartao(
+    'Fortnite',
+    'Em que ano foi lançado o Fortnite?',
+    '2017',
+)
+
+criaCartao(
+    'Seleção Brasileira',
+    'Quem é o artilheiro da seleção brasileira?',
+    'Neymar Jr',
+)
+
+criaCartao(
+    'Champions League',
+    'Quem é o artilheiro da champions?',
+    'Cristiano Ronaldo',
+)
+
+criaCartao(
+    'atleTIBA',
+    'Qual time tem mais vitórias no atleTIBA?',
+    'Coritiba',
+)
